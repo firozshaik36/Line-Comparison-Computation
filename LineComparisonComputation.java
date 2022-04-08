8e221b9c792fc3d7a2ba88b102496640e2052e82
firozshaik36 <firozshaik0007@gmail.com>
2022-04-08 22:20:11 +0530

    Checking:Equality of Two Lengths

--- LineComparisonComputation.java
+++ LineComparisonComputation.java
@@ -2,34 +2,36 @@ import java.util.Scanner;
 
 public class LineComparisonComputation
 {
-    public static void main(String args[])
-    {
-        int x1,x2,y1,y2;
+    public static void main(String args[]) {
+        int x1, x2, y1, y2;
 
-        double length;
+        double length1;
 
-        Scanner sc=new Scanner(System.in);
+
+        Scanner sc = new Scanner(System.in);
 
         System.out.println("enter x1 point");
 
-        x1=sc.nextInt();
+        x1 = sc.nextInt();
 
         System.out.println("enter y1 point");
 
-        y1=sc.nextInt();
+        y1 = sc.nextInt();
 
         System.out.println("enter x2point");
 
-        x2=sc.nextInt();
+        x2 = sc.nextInt();
 
         System.out.println("enter y2 point");
 
-        y2=sc.nextInt();
+        y2 = sc.nextInt();
 
-        length=Math.sqrt((x2-x1)^2 + (y2-y1)^2);
+        length1 = Math.sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
 
-        System.out.println("Length of Lines is : "+"("+x1+","+y1+"),"+"("+x2+","+y2+")===>"+length);
 
+        System.out.println("Length of Lines is : " + "(" + x1 + "," + y1 + ")," + "(" + x2 + "," + y2 + ")===>" + length1);
+        System.out.println(x1 + " == " + y1 + " : " + (x1 == y1));
+        System.out.println(x2 + " == " + y2 + " : " + (x2 == y2));
 
     }
 
