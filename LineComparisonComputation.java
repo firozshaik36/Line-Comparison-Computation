2eb5e2e9ad6c74c21573e996253b1611d4d2c546
firozshaik36 <firozshaik0007@gmail.com>
2022-04-08 17:46:27 +0530

    Welcome to Line COmparison Computation Program

---
+++ LineComparisonComputation.java
@@ -0,0 +1,18 @@
+
+
+
+public class LineComparisonComputation
+{
+    public static void main(String args[])
+    {
+               System.out.println("Welcome to Line Comparison Computation Program");
+
+    }
+
+}
+
+
+
+
+
+
